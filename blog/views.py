305adc7e4013904cda178f39e5d49efe26048342
Ricardolv <richardluizv@gmail.com>
2019-05-03 06:29:22 -0300

    Model Form

--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,16 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .forms import BlogPostForm
+from .forms import BlogPostModelForm
 from .models import BlogPost
 
-
-def blog_post_detail_page(request, slug):
-
-    # obj = BlogPost.objects.get(slug=slug)
-    # queryset = BlogPost.objects.filter(slug=slug)
-    # if queryset.count() == 0:
-    #     raise Http404
-    # obj = queryset.first()
-
-    obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog/detail.html'
-    context = {"object": obj} # {"title": obj.title}
-    return render(request, template_name, context)
-
 # CRUD
+
+# GET -> Retrieve / List
+
+# POST -> Create / Update / DELETE
+
+# Create Retrieve Update Delete
 
 def blog_post_list_view(request):
     # list out objects
@@ -33,10 +25,20 @@ def blog_post_list_view(request):
 def blog_post_create_view(request):
     # create objects
     # ? use a form
-    form = BlogPostForm(request.POST or None)
+
+    #  BlogPostForm
+    # form = BlogPostForm(request.POST or None)
+    # if form.is_valid():
+    #     obj = BlogPost.objects.create(**form.cleaned_data)
+    #     form = BlogPostForm()
+
+    #  BlogPostModelForm
+    form = BlogPostModelForm(request.POST or None)
     if form.is_valid():
-        obj = BlogPost.objects.create(**form.cleaned_data)
-        form = BlogPostForm()
+        obj = form.save(commit=False)
+        obj.save()
+
+        form = BlogPostModelForm()
 
     template_name = 'form.html'
     context = {'form': form}
