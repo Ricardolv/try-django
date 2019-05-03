912c7841b3520be091a85fe1a5b731f2afa4ac4d
Ricardolv <richardluizv@gmail.com>
2019-05-03 18:23:01 -0300

    Include with Arguments

--- core/views.py
+++ core/views.py
@@ -4,12 +4,12 @@ from django.template.loader import get_template
 # Dont Repeat Yourself = DRY
 
 from .forms import ConcatForm
+from blog.models import BlogPost
 
 def home_page(request):
     my_title = "hello there..."
-    context  =  {"title": "My title"}
-    if request.user.is_authenticated:
-        context  = {"title": my_title, "my_List": [1, 2, 3, 4 ,5]}
+    qs = BlogPost.objects.all()[:5]
+    context  =  {"title": "Welcome to Try Django", 'blog_list': qs}
     return render(request, "home.html", context)
 
 
