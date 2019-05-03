780ac1b58a195949d5e87a3c72393d236db3dabf
Ricardolv <richardluizv@gmail.com>
2019-05-03 04:51:14 -0300

    Routing the Views

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, re_path # url
 
 from blog.views import (
-    blog_post_detail_page,
+    blog_post_create_view,
+    blog_post_detail_view,
+    blog_post_update_view,
+    blog_post_delete_view,
     blog_post_list_view,
 )
 
@@ -31,9 +34,12 @@ from .views import (
 urlpatterns = [
     path('', home_page),
     path('blog/', blog_post_list_view),
-    path('blog/<str:slug>/', blog_post_detail_page),
-    #re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_page),
+    path('blog/<str:slug>/', blog_post_detail_view),
+    path('blog/<str:slug>/edit/', blog_post_update_view),
+    path('blog/<str:slug>/delete', blog_post_delete_view),
+    path('blog-new/', blog_post_create_view),
 
+    #re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_page),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('contact/', contact_page),
