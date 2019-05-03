d7965510d6f4789c0598b3ead7ca0fe5039d3c7b
Ricardolv <richardluizv@gmail.com>
2019-05-03 02:08:36 -0300

    Render Context in Templates

--- core/views.py
+++ core/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "hello_word.html")
+    my_title = "hello there..."
+    return render(request, "hello_word.html", {"title": my_title})
 
 
 def about_page(request):
-    return HttpResponse("<h1> About Us</h1>")
+    return render(request, "hello_word.html", {"title": "About"})
 
 
 def contact_page(request):
-    return HttpResponse("<h1> Contact Us</h1>")
+    return render(request, "hello_word.html", {"title": "Contact"})
