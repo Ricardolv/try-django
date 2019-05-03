ded92abd447714a4fd98df8265abc4cba7ae8fce
Ricardolv <richardluizv@gmail.com>
2019-05-03 04:41:35 -0300

    Blog Post List View

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, re_path # url
 
 from blog.views import (
-    blog_post_detail_page
+    blog_post_detail_page,
+    blog_post_list_view,
 )
 
 from .views import (
@@ -29,7 +30,7 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
-    #path('blog/', blog_post_detail_page),
+    path('blog/', blog_post_list_view),
     path('blog/<str:slug>/', blog_post_detail_page),
     #re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_page),
 
