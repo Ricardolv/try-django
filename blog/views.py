fb0ed03306966c501827685062d5ca69afb56b38
Ricardolv <richardluizv@gmail.com>
2019-05-03 05:16:24 -0300

    In App Templates

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import BlogPost
 
+
 def blog_post_detail_page(request, slug):
 
     # obj = BlogPost.objects.get(slug=slug)
@@ -13,7 +14,7 @@ def blog_post_detail_page(request, slug):
     # obj = queryset.first()
 
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
+    template_name = 'blog/detail.html'
     context = {"object": obj} # {"title": obj.title}
     return render(request, template_name, context)
 
@@ -23,7 +24,7 @@ def blog_post_list_view(request):
     # list out objects
     # could be search
     qs = BlogPost.objects.all() # queryset -> list of python object
-    template_name = 'blog_post_list.html'
+    template_name = 'blog/list.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
 
@@ -31,7 +32,7 @@ def blog_post_list_view(request):
 def blog_post_create_view(request):
     # create objects
     # ? use a form
-    template_name = 'blog_post_create.html'
+    template_name = 'blog/create.html'
     context = {'form': None}
     return render(request, template_name, context)
 
@@ -39,20 +40,20 @@ def blog_post_create_view(request):
 def blog_post_detail_view(request, slug):
     # 1 object -> detail view
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
+    template_name = 'blog/detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_update.html'
+    template_name = 'blog/update.html'
     context = {"object": obj , 'form': None}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_delete.html'
+    template_name = 'blog/delete.html'
     context = {"object": obj}
     return render(request, template_name, context)
