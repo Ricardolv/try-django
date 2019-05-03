632315f9c6b07275fab7bb958f092812a9838cf9
Ricardolv <richardluizv@gmail.com>
2019-05-03 02:25:09 -0300

    Stay DRY with Templates

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Dont Repeat Yourself = DRY
+
 def home_page(request):
     my_title = "hello there..."
     return render(request, "hello_word.html", {"title": my_title})
 
 
 def about_page(request):
-    return render(request, "hello_word.html", {"title": "About"})
+    return render(request, "about.html", {"title": "About"})
 
 
 def contact_page(request):
