577ac367300c196d32f0c15ed892d1dad4e344f8
Ricardolv <richardluizv@gmail.com>
2019-05-03 04:05:04 -0300

    A New Database Lookup Value

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(request, post_id):
+def blog_post_detail_page(request, slug):
 
-    #obj = BlogPost.objects.get(id=post_id)
-    obj = get_object_or_404(BlogPost, id=post_id)
+    #obj = BlogPost.objects.get(slug=post_id)
+    obj = get_object_or_404(BlogPost, slug=slug)
 
     template_name = 'blog_post_detail.html'
     context = {"object": obj} # {"title": obj.title}
