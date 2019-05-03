881bc8a5b83bbe3a21744293b2e40ae3f2177d51
Ricardolv <richardluizv@gmail.com>
2019-05-03 03:28:59 -0300

    Model in a View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import BlogPost
+
+def blog_post_detail_page(request):
+    obj = BlogPost.objects.get(id=1) # query -> database -> data -> django renders it
+    template_name = 'blog_post_detail.html'
+    context = {"object": obj} # {"title": obj.title}
+    return render(request, template_name, context)
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path # url
 
+from blog.views import (
+    blog_post_detail_page
+)
+
 from .views import (
     home_page,
     about_page,
@@ -25,6 +29,7 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
+    path('blog/', blog_post_detail_page),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('contact/', contact_page),
